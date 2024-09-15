7abe0ac81941ae871fccaa203a6f9306cccbd309
Igor Rodionov <rodionov0504@gmail.com>
2024-09-15 16:14:07 +0200

    Add Diary Create use case

---
+++ src/SensusJournal.Application/UseCases/Diarys/Create/DiaryCreateCommand.cs
@@ -0,0 +1,8 @@
+﻿using SensusJournal.Application.Abstractions;
+
+namespace SensusJournal.Application.UseCases.Diarys.Create;
+
+public record DiaryCreateCommand(string Name, string Details)
+    : ICommand<DiaryCreateResponse>
+{
+}
---
+++ src/SensusJournal.Application/UseCases/Diarys/Create/DiaryCreateCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using SensusJournal.Application.Abstractions;
+using SensusJournal.Application.Interfaces.Data;
+using SensusJournal.Core.Entities;
+
+namespace SensusJournal.Application.UseCases.Diarys.Create;
+
+public class DiaryCreateCommandHandler : ICommandHandler<DiaryCreateCommand, DiaryCreateResponse>
+{
+    private readonly ISensusJournalDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public DiaryCreateCommandHandler(ISensusJournalDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<DiaryCreateResponse>> Handle(DiaryCreateCommand request, CancellationToken cancellationToken = default)
+    {
+        // Create entity
+        var diary = _mapper.Map<Diary>(request);
+
+        try
+        {
+            await _dbContext.Diarys.AddAsync(diary);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            return Result<DiaryCreateResponse>.Failure(ex.Message);
+        }
+
+        return Result<DiaryCreateResponse>.Success(_mapper.Map<DiaryCreateResponse>(diary));
+    }
+}
